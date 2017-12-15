333df17f5611dcedcaf19b8de71e2fd08c368608
Mr.lee <lizisheng@kuaikuaidai.com>
2017-12-15 11:09:03 +0800

    first

---
+++ app/src/main/java/com/example/r/tablayoutdemo/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.r.tablayoutdemo;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements OnTabSelectListener{
+    private Context mContext = this;
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+    private final String[] mTitles = {
+            "热门", "iOS", "Android"
+            , "前端", "后端", "设计", "工具资源"
+    };
+    private MyPagerAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        for (String title : mTitles) {
+            mFragments.add(SimpleCardFragment.getInstance(title));
+        }
+
+        View decorView = getWindow().getDecorView();
+        ViewPager vp = ViewFindUtils.find(decorView, R.id.vp);
+        mAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        vp.setAdapter(mAdapter);
+        /** indicator圆角色块 */
+        SlidingTabLayout tabLayout_9 = ViewFindUtils.find(decorView, R.id.tl_9);
+        tabLayout_9.setViewPager(vp);
+
+        vp.setCurrentItem(4);
+
+    }
+
+    @Override
+    public void onTabSelect(int position) {
+
+    }
+
+    @Override
+    public void onTabReselect(int position) {
+
+    }
+
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitles[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/r/tablayoutdemo/OnTabSelectListener.java
@@ -0,0 +1,10 @@
+package com.example.r.tablayoutdemo;
+
+/**
+ * Created by r on 2017/12/14.
+ */
+
+public interface OnTabSelectListener {
+        void onTabSelect(int position);
+        void onTabReselect(int position);
+}
